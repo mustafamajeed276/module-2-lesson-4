47607500113bac351152c933836078729f5f2630
mustafa276 <mmustafamajeed1@gmail.com>
2024-09-04 12:46:40 +0500

    homework

---
+++ activity3.py
@@ -0,0 +1,11 @@
+num = int(input("enter the number : "))
+t = num
+numlen = 0
+while t>0:
+    numlen = numlen + 1
+    t = int(t/10)
+if numlen >= 4:
+    numlen = int(numlen/2)
+    chk = 0
+    while num>0:
+        rem = num%10
